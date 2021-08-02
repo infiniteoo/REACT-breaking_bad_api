03b252252d3ce403504866435da7f5e02f6f15ea
Troy Dorman <troydorman@gmail.com>
2021-08-02 07:32:48 -0600

    imported hooks, defined a couple states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import Header from "./components/ui/Header";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  
+
   return (
     <div className="container">
       <Header />
